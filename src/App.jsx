e1a9cbb5b59328bdd637050290b9ae433d1770f1
Tushar Gupta <tushargupta4870@gmail.com>
2023-12-08 16:05:29 +0530

    made the background component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import Background from './components/Background';
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
-export default App
+    <div className='w-full h-screen bg-zinc-800'>
+      <Background />
+    </div>
+  );
+};
+export default App;
---
+++ src/components/Background.jsx
@@ -0,0 +1,13 @@
+const Background = () => {
+  return (
+    <>
+      <div className='absolute top-[5%] w-full py-10 flex justify-center text-zinc-600 font-semibold text-xl '>
+        Documents
+      </div>
+      <h1 className=' absolute left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%] text-[12vw] leading-none tracking-tighter font-semibold text-zinc-900'>
+        Docs
+      </h1>
+    </>
+  );
+};
+export default Background;
